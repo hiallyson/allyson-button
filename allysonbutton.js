9631f7fa25cb470342ad6b7ef6025020fd35c0fe
hiallyson <36341805+hiallyson@users.noreply.github.com>
2018-02-26 00:48:40 -0500

    Add files via upload

---
+++ allysonbutton.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    // jQuery, find the hide ID and wait for the user to click its element (button).
+    $("#hide").click(function(){
+        // jQuery, when the user clicks the Hide button, find all p tags and hide them!
+        $("img").hide();
+    });
+
+
+ // jQuery, find the show ID and wait for the user to click its element (button).
+    $("#show").click(function(){
+        // jQuery, when the user clicks the Show button, reshow or keep showing all p tags!
+        $("img").show();
+    // that's it
+    });
+});
+
+
+
+
+// <img src="heartbutton.jpg" alt="We Heart Buttons"> // 
+
+
+
